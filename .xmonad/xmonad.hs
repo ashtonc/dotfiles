c2d67e178be56fd0935173f52d870e546d69a4a8
Ashton Charbonneau <ashton@ashtonc.ca>
2018-08-21 16:06:49 -0600

    Update xmonad layout
    Simplifies the xmonad layout and uses smartborders so that the gaps make
    more sense (no gaps with a single window, etc).

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -4,9 +4,29 @@
 
 import XMonad
 import XMonad.Hooks.DynamicLog
+import XMonad.Layout.NoBorders (smartBorders)
+import XMonad.Layout.Spacing (smartSpacing)
+
+-- No borders on fullscreen content, borders around boxes, borders of width 4
+myLayoutHook         = smartSpacing 1 $ smartBorders (layoutHook defaultConfig)
+myBorderWidth        = 4
+
+-- Border colors
+myNormalBorderColor  = "#272B34"
+myFocusedBorderColor = "#60AFFC"
+
+-- Change the mod key to the windows (super) key
+myModMask            = mod4Mask
+
+-- 
+--myLogHook            = 
 
 main = xmonad =<< xmobar defaultConfig
-	{ modMask            = mod4Mask
-	, borderWidth        = 3
-    , normalBorderColor  = "#272B34"
-    , focusedBorderColor = "#60AFFC" }
+	{ modMask            = myModMask
+    , layoutHook         = myLayoutHook
+--	, logHook            = myLogHook
+	, borderWidth        = myBorderWidth
+    , normalBorderColor  = myNormalBorderColor
+    , focusedBorderColor = myFocusedBorderColor }
+
+
